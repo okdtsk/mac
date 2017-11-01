1d52728bd8dcfbc2dffd9e1f1f7d50aa4a80f215
Tasuku Okuda <okdtsk@gmail.com>
2017-11-02 05:06:44 +0900

    Add playbook for each envronment

--- ansible-playbook/run.sh
+++ ansible-playbook/run.sh
@@ -2,4 +2,8 @@
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
+if [ ! -e ./playbook-each-environment.yml ]; then
+    echo "---" > ./playbook-each-environment.yml
+fi
+
 ansible-playbook -i hosts ./playbook.yml $@
