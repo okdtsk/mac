9fe0f29260ca519d393914a69d62ce66b1bc951d
Tasuku OKUDA <okdtsk@gmail.com>
2017-11-13 16:38:46 +0900

    Make configurable ansible runtime arguments

--- ansible-playbook/run.sh
+++ ansible-playbook/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+readonly ANSIBLE_OPTS="${@}"
+
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 if [ ! -e ./playbook-each-environment.yml ]; then
     echo "---" > ./playbook-each-environment.yml
 fi
 
-ansible-playbook -i hosts ./playbook.yml $@
+ansible-playbook -i hosts ${ANSIBLE_OPTS} ./playbook.yml $@
