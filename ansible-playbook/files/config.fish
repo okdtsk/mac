f6c713dc32f4985f4a6ec1e2a4648e59aa9544df
Tasuku OKUDA <okdtsk@gmail.com>
2017-11-24 15:20:36 +0900

    Add JAVA_HOME to use 1.8

--- ansible-playbook/files/config.fish
+++ ansible-playbook/files/config.fish
@@ -1,5 +1,7 @@
 set -U fish_user_paths $HOME/bin $fish_user_paths
 
+set -x JAVA_HOME (/usr/libexec/java_home -v 1.8)
+
 source ~/.config/fish/iterm2_shell_integration.fish
 
 set -x EDITOR /usr/local/bin/vim
